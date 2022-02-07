3aa7479c69e775f6ae8aff5507029d2990a7bf17
Matt Stewart <smtp.matt@gmail.com>
2022-02-07 23:13:01 +1100

    Updates

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -46,10 +46,10 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
   use "numToStr/Comment.nvim" -- Easily comment stuff use "windwp/nvim-autopairs"
-  use "kyazdani42/nvim-web-devicons"
-  use "kyazdani42/nvim-tree.lua"
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
+  use "kyazdani42/nvim-web-devicons"
+  use "kyazdani42/nvim-tree.lua"
 
   -- colour schemes
   use "lunarvim/colorschemes"
